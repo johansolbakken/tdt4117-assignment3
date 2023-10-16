1076f25063139e46959d780c2ef3d4460c9c7800
Johan Solbakken <johsol@stud.ntnu.no>
2023-10-16 12:26:28 +0200

    Some thigns

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
+import config
+import os
+
 def main():
-    print("onus")
+    assert os.path.exists(config.DATA_FOLDER), "Data folder not found"
 
 if __name__ == "__main__":
     main()
